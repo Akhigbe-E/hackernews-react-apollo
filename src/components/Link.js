a9695be46456963c122e94ad723ce38092dbf3cc
Akhigbe-E <akhigbemmanuel@gmail.com>
2019-07-06 01:46:04 +0100

    Load links from GraphQl Api

---
+++ src/components/Link.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Link = props => {
+  return (
+    <div>
+      {props.link.description} {props.link.url}
+    </div>
+  );
+};
+
+export default Link;
---
+++ src/components/LinkList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "./Link";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const LinkList = props => {
+  const FEED_QUERY = gql`
+    {
+      feed(filter: "", skip: 0, first: 7) {
+        links {
+          id
+          url
+          description
+        }
+      }
+    }
+  `;
+  return (
+    <div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching...</div>;
+          if (error) return <div>Something Went wrong...</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map(link => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    </div>
+  );
+};
+
+export default LinkList;
