b776a1693199587ee825c25e9c3c81e1d6765fbc
Akhigbe-E <akhigbemmanuel@gmail.com>
2019-07-09 15:36:56 +0100

    Subscription funuctionality added

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -24,6 +24,50 @@ export const FEED_QUERY = gql`
     }
   }
 `;
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 export const LinkList = props => {
   const _updateCacheAfterVote = (store, createdVote, id) => {
     const data = store.readQuery({ query: FEED_QUERY });
@@ -33,12 +77,39 @@ export const LinkList = props => {
     console.log(data);
     store.writeQuery({ query: FEED_QUERY, data });
   };
+  const _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, dat) => {
+        const subsciptionData = dat.subscriptionData;
+        if (!subsciptionData.data) return prev;
+        console.log(prev.feed.links);
+        const newLink = subsciptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        });
+      }
+    });
+  };
+  const _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    });
+  };
   return (
     <div>
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching...</div>;
           if (error) return <div>Something Went wrong...</div>;
+          _subscribeToNewLinks(subscribeToMore);
+          _subscribeToNewVotes(subscribeToMore);
           const linksToRender = data.feed.links;
 
           return (
